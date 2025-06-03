488aebb6595ed5ba16aa44ec44ff394f36114d98
andrewknotts-en <andrew.knotts@eranova.ai>
2025-06-03 14:04:46 -0400

    updates

--- JS/base.js
+++ JS/base.js
@@ -223,3 +223,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+  const hamburgerBtn = document.getElementById('hamburger-btn');
+  const mobileNav = document.getElementById('mobile-nav');
+
+  hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('open');
+    mobileNav.classList.toggle('open');
+  });
